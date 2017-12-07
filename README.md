69ba86e799a3beec6cfee04cf40939bfc7c061f4
Alexandre Briet <alexandrerusilas@gmail.com>
2017-12-07 16:31:15 -0200

    Editando Read.me e adicionando voxus.sql

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# desafio-voxus
+# Desafio Voxus
+
+Realizado o desafio proposto pela Voxus de desenvolver um Dashboard de Tasks.
+
+**Tecnologias Utilizadas** 
+
+ 
+
+`PHP | jQuery | AJAX | HTML5 | CSS5`\
+`BootStrap | FontAwesome`
+
+
+
+
+Para realizar o teste será necessario que o banco de dados utilizado no desenvolvimento '**voxus.sql**' que se encontra na pasta raiz.
+
+
+Após configurar o banco de dados deve configurar a conexão  no arquivo  ' **Config.php** ' que se encontra do diretorio: _./inc/Config.php_
+
+
+**Programas utilizados**\
+`WampServer | MySQL Worckbench | PhpStorm | Notepad++ | Git`
+
+
+
---
+++ voxus.sql
@@ -0,0 +1,117 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: voxus
+-- ------------------------------------------------------
+-- Server version	5.7.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `anexos`
+--
+
+DROP TABLE IF EXISTS `anexos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `anexos` (
+  `anexo_id` int(11) NOT NULL AUTO_INCREMENT,
+  `anexo_dir` varchar(500) NOT NULL,
+  `anexo_type` enum('Arquivo','Imagem') NOT NULL,
+  `task_id` int(11) NOT NULL,
+  PRIMARY KEY (`anexo_id`,`task_id`),
+  KEY `fk_anexos_tasks_idx` (`task_id`),
+  CONSTRAINT `fk_anexos_tasks` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `anexos`
+--
+
+LOCK TABLES `anexos` WRITE;
+/*!40000 ALTER TABLE `anexos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `anexos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tasks` (
+  `task_id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_name` varchar(255) NOT NULL,
+  `task_description` varchar(3000) DEFAULT NULL,
+  `task_level` int(1) NOT NULL,
+  `task_status` enum('Nova','Em processo','Pronta','Finalizada') NOT NULL DEFAULT 'Nova',
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`task_id`,`user_id`),
+  KEY `fk_tasks_users1_idx` (`user_id`),
+  CONSTRAINT `fk_tasks_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `oauth_provider` varchar(255) NOT NULL,
+  `oauth_uid` varchar(255) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `locale` varchar(10) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `level` enum('Membro','Moderador','Administrador') NOT NULL DEFAULT 'Membro',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-12-07 16:18:10
